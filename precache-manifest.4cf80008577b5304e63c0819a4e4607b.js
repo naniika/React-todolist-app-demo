c87a35f728331ae5035fc198e5daaf371ced101d
Naniika <naniikagh@gmail.com>
2019-12-11 09:45:13 +0700

    Updates

---
+++ precache-manifest.4cf80008577b5304e63c0819a4e4607b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a44bc1d5957b05b52c33992e832631",
+    "url": "/React-todolist-app-demo/index.html"
+  },
+  {
+    "revision": "f3d6dfef772885954ea8",
+    "url": "/React-todolist-app-demo/static/css/main.b3c27e49.chunk.css"
+  },
+  {
+    "revision": "dd289b288d76a0a21427",
+    "url": "/React-todolist-app-demo/static/js/2.e0a8cfcb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-todolist-app-demo/static/js/2.e0a8cfcb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3d6dfef772885954ea8",
+    "url": "/React-todolist-app-demo/static/js/main.8f0d03c9.chunk.js"
+  },
+  {
+    "revision": "286b124ca0add2d6a0dd",
+    "url": "/React-todolist-app-demo/static/js/runtime-main.2666ff76.js"
+  }
+]);
